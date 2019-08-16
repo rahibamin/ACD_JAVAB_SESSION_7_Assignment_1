3be0ff607aa8a7bf82b6724561c359566f28bb98
rahibamin <54030063+rahibamin@users.noreply.github.com>
2019-08-16 10:56:06 -0400

    Add files via upload

---
+++ stringConvert.java
@@ -0,0 +1,18 @@
+package Session7.Assignment1;
+
+public class stringConvert {
+
+	public static void main(String[] args) {
+		
+		int x = 100;
+		String nc= String.valueOf(x);
+		
+		
+		System.out.println("Your converted String is: " +nc);
+
+	}
+
+	
+	
+
+}
